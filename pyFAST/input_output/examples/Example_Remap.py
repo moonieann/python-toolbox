746a849c043e2e8dc1727f80b5ba72419cf9aeba
anwf <anwenfeng1997@163.com>
2021-11-22 17:01:12 +0800

    change some formats

--- pyFAST/input_output/examples/Example_Remap.py
+++ pyFAST/input_output/examples/Example_Remap.py
@@ -10,18 +10,18 @@ import pyFAST.input_output as io
 import pyFAST.input_output.postpro as postpro
 
 if __name__ == '__main__':
-    ColumnMap={
-      'WS_[m/s]'         : '{Wind1VelX_[m/s]}'             , # create a new column from existing one
-      'RtTSR_[-]'        : '{RtTSR_[-]} * 2  +  {RtAeroCt_[-]}'    , # change value of column
-      'RotSpeed_[rad/s]' : '{RotSpeed_[rpm]} * 2*np.pi/60 ', # new column [rpm] -> [rad/s]
+    ColumnMap = {
+      'WS_[m/s]'         : '{Wind1VelX_[m/s]}'             ,         # create a new column from existing one
+      'RtTSR_[-]'        : '{RtTSR_[-]} * 2  +  {RtAeroCt_[-]}'    ,      # change value of column
+      'RotSpeed_[rad/s]' : '{RotSpeed_[rpm]} * 2*np.pi/60 ',           # new column [rpm] -> [rad/s]
     }
 
     # Get current directory so this script can be called from any location
-    MyDir=os.path.dirname(__file__)
+    MyDir = os.path.dirname(__file__)
     # --- Step 1: Read an openfast output file
-    outFile = os.path.join(MyDir,'../../../data/example_files/fastout_allnodes.outb')
+    outFile = os.path.join(MyDir, '../../../data/example_files/fastout_allnodes.outb')
     df = io.fast_output_file.FASTOutputFile(outFile).toDataFrame()
     # Change columns based on formulae, potentially adding new columns
     df = postpro.remap_df(df, ColumnMap)
-
-    #df.to_csv('_Out.csv', index=False)
+    # print(df)
+    # df.to_csv('_Out.csv', index=False)
