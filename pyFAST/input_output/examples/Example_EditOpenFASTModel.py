f89c10633b83f0d6bb13dd48f014fc1cb900d1e6
anwf <anwenfeng1997@163.com>
2021-11-25 13:22:32 +0800

    make some changes to run and merged with branch dev

--- pyFAST/input_output/examples/Example_EditOpenFASTModel.py
+++ pyFAST/input_output/examples/Example_EditOpenFASTModel.py
@@ -18,70 +18,67 @@ import matplotlib.pyplot as plt
 
 
 # Get current directory so this script can be called from any location
-MyDir=os.path.dirname(__file__)
+MyDir = os.path.dirname(__file__)
 
 
 # --- ElastoDyn file (geometry, inertias, etc.)
 # Read some geometrical data, modify them, write a new file
 print('------------------- ElastoDyn -------------------------------------')
-EDFilename = os.path.join(MyDir,'../../../data/NREL5MW/onshore/NREL5MW_ED_Onshore.dat')
+EDFilename = os.path.join(MyDir, '../../../data/NREL5MW/onshore/NREL5MW_ED_Onshore.dat')
 ED = FASTInputFile(EDFilename)
-print('> Keys:',ED.keys())
-print('> Hub radius: ',ED['HubRad'])
-print('> Tip radius: ',ED['TipRad'])
-print('> Hub mass:   ',ED['HubMass'])
-ED['TipRadius'] = 64 # Modifying the data
-#ED.write('_NewFile.dat') # write a new file with modified data
+print('> Keys:', ED.keys())
+print('> Hub radius: ', ED['HubRad'])
+print('> Tip radius: ', ED['TipRad'])
+print('> Hub mass:   ', ED['HubMass'])
+ED['TipRadius'] = 64                       # Modifying the data
+# ED.write('_NewFile.dat') # write a new file with modified data
 
 
 # --- AeroDyn blade 
 # Read an AeroDyn blade file, convert to a dataframe for convenience
 print('------------------- AeroDyn blade ---------------------------------')
-bldFilename = os.path.join(MyDir,'../../../data/NREL5MW/5MW_Baseline/NRELOffshrBsline5MW_AeroDyn_blade.dat')
+bldFilename = os.path.join(MyDir, '../../../data/NREL5MW/5MW_Baseline/NRELOffshrBsline5MW_AeroDyn_blade.dat')
 bld = FASTInputFile(bldFilename)
-print('> Keys:',bld.keys())
-data = bld['BldAeroNodes']   # Accessing the blade nodal data directly (numpy array, can be modified)
-bld['BldAeroNodes'][:,5] *=2 # Modify (multiply chord by 2)
-df  = bld.toDataFrame()      # Using a dataframe for easy export/manipulation
-print('> Dataframe columns:',df.columns.values)
-df.plot('BlSpn_[m]','BlChord_[m]')
+print('> Keys:', bld.keys())
+data = bld['BldAeroNodes']          # Accessing the blade nodal data directly (numpy array, can be modified)
+bld['BldAeroNodes'][:, 5] *= 2      # Modify (multiply chord by 2)
+df = bld.toDataFrame()              # Using a dataframe for easy export/manipulation
+print('> Dataframe columns:', df.columns.values)
+df.plot('BlSpn_[m]', 'BlChord_[m]')
 
 # --- Profile file 
 print('------------------- Profile data ----------------------------------')
-PFilename = os.path.join(MyDir,'../../../data/NREL5MW/5MW_Baseline/Airfoils/DU21_A17.dat')
+PFilename = os.path.join(MyDir, '../../../data/NREL5MW/5MW_Baseline/Airfoils/DU21_A17.dat')
 P = FASTInputFile(PFilename)
-print('> Keys:',P.keys())
-data = P['AFCoeff']  # Accessing the polar data directly (numpy array, can be modified)
-df = P.toDataFrame() # Using a dataframe for easy export/manipulation
-print('> Dataframe columns:',df.columns.values)
-df.plot('Alpha_[deg]','Cl_[-]')
+print('> Keys:', P.keys())
+data = P['AFCoeff']       # Accessing the polar data directly (numpy array, can be modified)
+df = P.toDataFrame()      # Using a dataframe for easy export/manipulation
+print('> Dataframe columns:', df.columns.values)
+df.plot('Alpha_[deg]', 'Cl_[-]')
 
 
 # --- ElastoDyn blade file 
 print('------------------- ElastoDyn blade -------------------------------')
-bldFilename = os.path.join(MyDir,'../../../data/NREL5MW/5MW_Baseline/NRELOffshrBsline5MW_Blade.dat')
+bldFilename = os.path.join(MyDir, '../../../data/NREL5MW/5MW_Baseline/NRELOffshrBsline5MW_Blade.dat')
 bld = FASTInputFile(bldFilename)
-print('> Keys:',bld.keys())
+print('> Keys:', bld.keys())
 data = bld['BldProp']   # Accessing the blade nodal data directly (numpy array, can be modified)
 df = bld.toDataFrame()  # Using a dataframe for easy export/manipulation
-print('> Dataframe columns:',df.columns.values)
-df.plot('BlFract_[-]','BMassDen_[kg/m]')
+print('> Dataframe columns:', df.columns.values)
+df.plot('BlFract_[-]', 'BMassDen_[kg/m]')
 
 # --- BeamDyn blade file 
 print('------------------- BeamDyn blade file ----------------------------')
-BDFilename = os.path.join(MyDir,'../../../data/NREL5MW/5MW_Baseline/NRELOffshrBsline5MW_BeamDyn_Blade.dat')
+BDFilename = os.path.join(MyDir, '../../../data/NREL5MW/5MW_Baseline/NRELOffshrBsline5MW_BeamDyn_Blade.dat')
 BD = FASTInputFile(BDFilename)
-print('> Keys:',BD.keys())
-BP_ref = BD['BeamProperties'] # access data directly (dictionary)
+print('> Keys:', BD.keys())
+BP_ref = BD['BeamProperties']      # access data directly (dictionary)
 # Modify blade property in for loop, write new files
-#for loop:
+# for loop:
 #    BP     = BP_ref.copy()
 #    BP['K']= BP['K']*i # modify some of the dictionary
 #    BD['BeamProperties'] =BP
 #    BD.write('newfile')
-
-
-
 
 
 if __name__ == '__main__':
